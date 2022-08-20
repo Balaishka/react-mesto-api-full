852efa81ed4aa4054b2c40b73b73eeb1afe26423
Balai Mikheeva <balaishka@yandex.ru>
2022-08-20 22:57:50 +0300

    feat: first

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const auth = require('./middlewares/auth');
 const NotFoundError = require('./errors/not-found-err');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(bodyParser.json());
