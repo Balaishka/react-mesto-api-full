35d66e321d448e55477d38937fe9e51ca45800c6
Balai Mikheeva <balaishka@yandex.ru>
2022-08-26 16:23:13 +0300

    feat: env

--- backend/helpers/jwt-sign.js
+++ backend/helpers/jwt-sign.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
-const { JWT_SECRET } = require('../configs/index');
+// const { JWT_SECRET } = require('../configs/index');
+const { NODE_ENV, JWT_SECRET } = process.env;
 
-const jwtSign = (id) => jwt.sign({ _id: id }, JWT_SECRET, { expiresIn: '7d' });
+const jwtSign = (id) => jwt.sign({ _id: id }, NODE_ENV === 'production' ? JWT_SECRET : 'balai-secret', { expiresIn: '7d' });
 
 module.exports = jwtSign;
--- backend/helpers/jwt-verify.js
+++ backend/helpers/jwt-verify.js
@@ -1,9 +1,10 @@
 const jwt = require('jsonwebtoken');
-const { JWT_SECRET } = require('../configs/index');
+// const { JWT_SECRET } = require('../configs/index');
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 const jwtVerify = (token) => {
   try {
-    return jwt.verify(token, JWT_SECRET);
+    return jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'balai-secret');
   } catch (err) {
     return false;
   }
